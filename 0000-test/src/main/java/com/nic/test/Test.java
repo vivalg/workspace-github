9b45c172de89e2987d2514f2844118a7a5b6335e
vivalg <vivalg@sina.com>
2013-04-22 02:16:27 -0700

    modified

--- 0000-test/src/main/java/com/nic/test/Test.java
+++ 0000-test/src/main/java/com/nic/test/Test.java
@@ -3,7 +3,7 @@ package com.nic.test;
 public class Test {
 
     public static void main(String[] args) {
-        Integer i = 32 & 40;
+        Integer i = 32 & 1023;
         System.out.println(i);
     }
 
