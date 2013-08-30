fe6e0c7fd343a513cedef9d337ca9dc3cbfa145e
vivalg <vivalg@sina.com>
2013-08-29 19:00:10 -0700

    Quick Sort

--- 0000-test/src/main/java/com/nic/algorithm/QuickSort.java
+++ 0000-test/src/main/java/com/nic/algorithm/QuickSort.java
@@ -1,24 +1,59 @@
 package com.nic.algorithm;
 
+import java.util.Arrays;
+
 public class QuickSort {
 
     public static void main(String[] args) {
-
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i + ":" + i / 2);
-        }
+        int[] arr = generateRandomArray(10);
+        System.out.println(Arrays.toString(arr));
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
     }
 
-    public void quickSort(int[] arr, int beginPos, int len) {
-        if (len <= 1) {
-            return;
+    public static int[] generateRandomArray(int length) {
+        int[] arr = new int[length];
+        for (int i = 0; i < length; i++) {
+            long randNum = Math.round((Math.random() * 100));
+            arr[i] = (int) randNum;
         }
-        int pivotPos = beginPos + len / 2;
+        return arr;
+    }
+
+    public static void quickSort(int[] arr, int beginPos, int endPos) {
+        int pivotPos = beginPos + (endPos - beginPos) / 2;
         int pivotElem = arr[pivotPos];
+
+        int i = beginPos;
+        int j = endPos;
+
+        while (i <= j) {
+            while (arr[i] < pivotElem) {
+                i++;
+            }
+
+            while (arr[j] > pivotElem) {
+                j--;
+            }
+
+            if (i <= j) {
+                swap(arr, i, j);
+                i++;
+                j--;
+            }
+        }
+
+        if (j > beginPos) {
+            quickSort(arr, beginPos, j);
+        }
+
+        if (i < endPos) {
+            quickSort(arr, j, endPos);
+        }
+
     }
 
-
-    public void swap(int[] arr, int m, int n) {
+    public static void swap(int[] arr, int m, int n) {
         int temp = arr[m];
         arr[m] = arr[n];
         arr[n] = temp;
