0e745f321cb7d0cc90e39d6ef4204d7696ed8696
jack <vivalg@sina.com>
2013-09-22 08:52:18 +0800

    first commit
    	modified:   0000-test/src/main/java/com/nic/test/Test.java
    	new file:   README

--- 0000-test/src/main/java/com/nic/test/Test.java
+++ 0000-test/src/main/java/com/nic/test/Test.java
@@ -5,6 +5,7 @@ public class Test {
     public static void main(String[] args) {
         Integer i = 32 & 1023;
         System.out.println(i);
+        System.out.println("hello there");
     }
 
 }
